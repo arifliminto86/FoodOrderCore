60a1659300ec689df71f458b77c448b1baf2d7b2
Arif Liminto <arifliminto86@hotmail.com>
2019-03-25 16:13:15 +0800

    update controller

--- WebMvcSignalrCore/Controllers/HomeController.cs
+++ WebMvcSignalrCore/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebMvcSignalrCore.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }  
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
     }
 }
